b8ccdadfcf48fcc75a1991f1e9e8c530ed3bb29d
Simon Tudge <sjtudge@gmail.com>
2016-01-17 23:51:16 +0000

    Added some basic character stripping for security

--- php/make_plan.php
+++ php/make_plan.php
@@ -3,7 +3,14 @@
 	//Given the imputs from the get method this calculates the running
 	//plan based on the input of the user
 	require("plan.php");
-	$thePlan = new Plan( $_GET["weeks"], $_GET["distance"], $_GET["totalTime"], $_GET["days"] );
+
+	//This stops anyone from inserting scripts into this script
+	$weeks =  htmlspecialchars($_GET["weeks"]);
+	$distance = htmlspecialchars( $_GET["distance"] );
+	$totalTime = htmlspecialchars($_GET["totalTime"]);
+	$days = htmlspecialchars( $_GET["days"] );
+
+	$thePlan = new Plan( $weeks, $distance, $totalTime, $days );
 	$thePlan->describe();
 
 ?>
